4eccefe13d6737b5603557f1bdde1a6d8c8bca2a
gustave <gustaveploix.pro@gmail.com>
2021-09-01 18:55:11 +0200

    created boilerplate to work

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { 
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+import ZipSelection from './pages/ZipSelection';
+import Dashboard from './pages/Dashboard'
+
+import './app.scss'
+
+export default function App (){
+
+    const [zip, setZip] = useState()
+
+    return(
+
+        <Router>
+            <main>
+                <Switch>
+                    <Route path="/" exact>
+                        <ZipSelection zip={zip} setZip={setZip}/>
+                    </Route>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                </Switch>
+            </main>
+        </Router>
+
+    )
+
+}
