5caac3e3e8e40e96289ca2fd8dda5cc68848fa3c
gustave <gustaveploix.pro@gmail.com>
2021-09-02 19:31:58 +0200

    set transition when leave the landing page

--- src/components/ZipForm/index.jsx
+++ src/components/ZipForm/index.jsx
@@ -1,46 +1,50 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
+import { useHistory } from "react-router";
+import "./zipForm.scss";
 
-import './zipForm.scss'
+export default function ZipForm({ zip, setZip }) {
+  const zipRegex = new RegExp(/[0-9]{5}/g);
 
-export default function ZipForm ({ zip, setZip }){
+  let history = useHistory();
 
-    const zipRegex = new RegExp(/[0-9]{5}/g)
+  let submit = useRef()
 
-    const [valid, setValid] = useState(true)
+  const [valid, setValid] = useState(true);
 
-    const handleChange = (e) => {
-        setZip(e.target.value);
+  const handleChange = (e) => {
+    setZip(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    if (zipRegex.test(zip)) {
+      e.preventDefault();
+      submit.current.className = "submit transition"
+      const timer = setTimeout(() => {
+        history.push(`/dashboard/${zip}`);
+        e.preventDefault()
+        console.log("okay");
+      }, 1000);
+      return () => clearTimeout(timer);
+    } else {
+      e.preventDefault();
+      console.log("nope");
+      setValid(false);
     }
-    
-    const handleSubmit = (e) => {
-        
-        if(zipRegex.test(zip)){
-            console.log("ok");
-            setValid(true);
-        } else {
-            e.preventDefault()
-            console.log("nope");
-            setValid(false)
-        }
-    }
+  };
 
-    return(
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className={valid ? "input" : "input error"}
+        placeholder="entrez votre code postal"
+        onChange={handleChange}
+        type="text"
+      />
 
-        <form action={`/dashboard/${zip}`}
-         onSubmit={handleSubmit}>
-
-            <input
-            className={valid ? "input" : "input error"}
-            placeholder="entrez votre code postal"
-            onChange={handleChange}
-            type="text" />
-
-            <input className="submit"
-            type="submit" value="=>" />
-            <p className={valid ? "invisible" : "visible"}>Veuillez entrer un code postal valide</p>
-        
-        </form>
-
-    )
-
+      <input className="submit" ref={submit} type="submit" value="=>" />
+      <p className={valid ? "invisible" : "visible"}>
+        Veuillez entrer un code postal valide
+      </p>
+    </form>
+  );
 }
