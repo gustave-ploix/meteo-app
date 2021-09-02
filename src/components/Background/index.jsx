3108d71df71c0c08f709d0fd7001d078f3bba577
gustave <gustaveploix.pro@gmail.com>
2021-09-02 14:22:30 +0200

    set up changing background

--- src/components/Background/index.jsx
+++ src/components/Background/index.jsx
@@ -2,38 +2,61 @@ import React from "react";
 
 import './background.scss'
 
-export default function Background({ sunDown, sunUp }) {
+export default function Background({ sunDown, sunUp, temp, clouds }) {
 
   const date = new Date();
-  const hour = date.getHours();
+//   const hour = date.getHours();
+    const hour = 7
   
+    const cloudPercentage = parseInt(clouds)
+    const temperature = parseInt(temp);
+
   let sunset = parseInt(sunDown)
   let classname = "";
 
-    if(sunset + 1 == hour){
-        classname = "plusOne"
-    }else if(sunset + 2 === hour) {
-        classname = "plusTwo"
-    }else if(sunset + 3 === hour){
-        classname = "plusThree"
-    }else if(sunset + 4 === hour){
-        classname = "plusFour"
-    }else if(sunset + 5 === hour){
-        classname = "plusFive"
-    }else if(sunset + 6 === hour){
-        classname = "plusSix"
-    }else if(sunset + 7 === hour){
-        classname = "plusSeven"
-    }else if(sunset + 8 === hour){
-        classname = "plusEight"
-    }else if(sunset + 9 === hour){
-        classname = "plusNine"
+  const sunrise = parseInt(sunUp);
+
+    const journey = () => {
+        if(cloudPercentage >= 0 && cloudPercentage <= 10){
+            classname="cloudZero"
+        }else if(cloudPercentage >= 10 && cloudPercentage <= 20) {
+            classname="cloudOne"
+        }else if(cloudPercentage >= 20 && cloudPercentage <= 30) {
+            classname="cloudThwo"
+        }else if(cloudPercentage >= 30 && cloudPercentage <= 40) {
+            classname="cloudThree"
+        }else if(cloudPercentage >= 40 && cloudPercentage <= 50) {
+            classname="cloudFour"
+        }else if(cloudPercentage >= 50 && cloudPercentage <= 60) {
+            classname="cloudFive"
+        }else if(cloudPercentage >= 60 && cloudPercentage <= 70) {
+            classname="cloudSix"
+        }else if(cloudPercentage >= 70 && cloudPercentage <= 80) {
+            classname='cloudSeven'
+        }else if(cloudPercentage >= 80 && cloudPercentage <= 90) {
+            classname="cloudEight"
+        }else if(cloudPercentage >= 90 && cloudPercentage <= 101) {
+            classname="cloudNine"
+        }
+    }
+
+    const night = () => {
+        if (hour >= sunset + 2 && hour <= sunrise) {
+        }
+    }
+    
+    
+    
+    if(hour >= sunrise + 2 && hour <= sunset) {
+        journey()
+        console.log("journey");
+    }else{
+        classname="night"
     }
 
 
-
   return(
-      <div className={classname}>
+      <div className={`bg ${classname}`}>
           {/* {classname} */}
       </div>
   )
